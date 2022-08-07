973696df43153d0880c3e698cb9f799ea2f033d8
zh-kai <zh_kai0531@163.com>
2022-08-07 16:28:50 +0800

    fix: mobile & dark mode

--- docs/.vitepress/components/animal-crossing/fish.vue
+++ docs/.vitepress/components/animal-crossing/fish.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="py-3 px-6 my-6 border rounded-full">
-    <input v-model="keyword" placeholder="输入鱼类名称" class="w-full" />
+  <div class="my-6 border rounded-full">
+    <input
+      v-model="keyword"
+      placeholder="输入鱼类名称"
+      class="w-full py-3 px-6 text-sm"
+    />
   </div>
-  <div class="h-32 grid grid-cols-6 overflow-auto">
+  <div class="h-128 grid grid-cols-3 lg:grid-cols-6 gap-6 overflow-auto">
     <div v-for="f in fishList" :key="f.name" class="flex flex-col items-center">
-      <img :src="withBase(imagePrefix + f.image)" class="w-16 h-16" />
-      <span class="mb-1 text-sm">{{ f.name }}</span>
-      <div class="flex items-center text-blue-900 text-opacity-50 text-xs">
-        <img src="/animal-crossing/money.png" class="w-3 h-3 mr-0.5" />
+      <img
+        :src="withBase(imagePrefix + f.english + imageSuffix)"
+        class="w-16 h-16"
+      />
+      <span class="mb-1 text-xs">{{ f.name }}</span>
+      <div
+        class="flex items-center text-blue-900 text-opacity-50 text-xs dark:text-blue-100 dark:text-opacity-50"
+      >
+        <img src="/animal-crossing/money.png" class="w-3.5 h-3.5 mr-0.5" />
         <span>{{ f.price }}</span>
       </div>
     </div>
@@ -18,7 +27,9 @@
 import { ref, computed } from "vue";
 import { Fish } from "../../constants/animal-crossing";
 import { withBase } from "vitepress";
+
 const imagePrefix = "animal-crossing/";
+const imageSuffix = ".png";
 
 const keyword = ref("");
 const fishList = computed(() =>
--- docs/.vitepress/constants/animal-crossing.js
+++ docs/.vitepress/constants/animal-crossing.js
@@ -1,38 +1,56 @@
 export const Fish = [
   {
-    image: "ji-yu.png",
+    english: "ji-yu",
     name: "鲫鱼",
     pinyin: "jiyu",
     price: 160,
   },
   {
-    image: "ke-dou.png",
+    english: "ke-dou",
     name: "蝌蚪",
     pinyin: "kedou",
     price: 100,
   },
   {
-    image: "qing-wa.png",
+    english: "qing-wa",
     name: "青蛙",
     pinyin: "qingwa",
     price: 120,
   },
   {
-    image: "zhu-jia-yu.png",
+    english: "zhu-jia-yu",
     name: "竹荚鱼",
     pinyin: "zhujiayu",
     price: 150,
   },
   {
-    image: "lan-sai-tai-yang-yu.png",
+    english: "lan-sai-tai-yang-yu",
     name: "蓝腮太阳鱼",
     pinyin: "lansaitaiyangyu",
     price: 180,
   },
   {
-    image: "xi-ge.png",
+    english: "xi-ge",
     name: "溪哥",
     pinyin: "xige",
     price: 180,
+  },
+  {
+    english: "crawfish",
+    name: "淡水龙虾",
+    pinyin: "danhuilongxia",
+    price: 200,
+  },
+  {
+    english: "anchovy",
+    name: "凤尾鱼",
+    pinyin: "fengweiyu",
+    price: 200,
+  },
+  {
+    english: "dace",
+    name: "雅罗鱼",
+    pinyin: "yaluoyu",
+    price: 200,
   },
 ];
